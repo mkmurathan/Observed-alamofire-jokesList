b5059324cded8550b6ef552b2e240eebdcf1d4e7
Murathan Karagöz <murathankaragooz@hotmail.com>
2023-08-29 12:48:25 +0300

    Initial Commit

---
+++ JokeSwiftUI/JokeSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  JokeSwiftUIApp.swift
+//  JokeSwiftUI
+//
+//  Created by Murathan karagöz on 29.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct JokeSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
